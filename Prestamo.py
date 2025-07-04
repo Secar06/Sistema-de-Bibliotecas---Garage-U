e7e2493677e2c792c49ef02dd026a43c175c65fe
Alejandro <elkinalejandro.lopez@udea.edu.co>
2025-07-04 16:09:25 -0500

    Crea clase Prestamo.py

---
+++ Prestamo.py
@@ -0,0 +1,11 @@
+class Prestamo:
+    """
+    Esta clase representa un prestamo de un recurso a un usuario.
+
+    """
+    def __init__(self, id, id_libro, id_usuario, fecha_prestamo, fecha_devolucion):
+            self.id = id
+            self.id_libro = id_libro
+            self.id_usuario = id_usuario
+            self.fecha_prestamo = fecha_prestamo
+            self.fecha_devolucion = fecha_devolucion
