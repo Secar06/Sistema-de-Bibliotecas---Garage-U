1316c8ea08775eb601098b90587d87b372645ea3
Sebas <sebastian.cardona7@udea.edu.co>
2025-07-07 16:58:01 -0500

    se corrigio error menor

--- Prestamo.py
+++ Prestamo.py
@@ -11,7 +11,7 @@ class Prestamo:
             self.id_usuario = id_usuario
             self.fecha_prestamo = fecha_prestamo
             self.fecha_devolucion_estimada = fecha_devolucion_estimada
-            self-fecha_devolucion = fecha_devolucion
+            self.fecha_devolucion = fecha_devolucion
 
 
     def get_id(self):
